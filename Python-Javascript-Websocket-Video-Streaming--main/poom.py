471d28b4aeb55052a19fdd011f8413fe8f8d4b5e
Kamo72 <agag3932@gmail.com>
2023-12-27 10:39:14 +0900

    231227commit

--- Python-Javascript-Websocket-Video-Streaming--main/poom.py
+++ Python-Javascript-Websocket-Video-Streaming--main/poom.py
@@ -8,10 +8,11 @@ import uvicorn
 from collections import  defaultdict
 import os
 import threading 
-app = FastAPI()
+app : FastAPI = FastAPI()
 app.mount('/static', StaticFiles(directory='static'), name='static')
- # (admin)
-sio = socketio.AsyncServer(async_mode='asgi',  
+
+# 비동기 서버 생성
+sio : socketio.AsyncServer = socketio.AsyncServer(async_mode='asgi',  
                           credits=True,
                            cors_allowed_origins = [
                             
@@ -22,107 +23,124 @@ sio = socketio.AsyncServer(async_mode='asgi',
                            
                            ])  
 
-sio.instrument(auth=False
-               #{'username':'WB38' , 'password':os.environ['WB38']}
-                     )
- 
+#관리자 모드 인증 설정
+# sio.instrument(auth=False) # 권한 없이 접속하기
+sio.instrument({'username':'WB38' , 'password':os.environ['WB38']})
 
- #WB38                           your_password
-
+#socketIO와 FastAPI를 합치기
 combined_asgi_app = socketio.ASGIApp(sio, app)
- 
-manager = sio.manager
-#clientsocket = socket.socket(socket.AF_INET ,socket.SOCK_STREAM)
-#host = '127.0.0.1'
-#port =  6000
-#clientsocket.connect((host,port))
 
- 
-rooms = defaultdict(dict) # 방에 대한 모든 정보를 담음 rooms[room] 에서 키를 뽑아서 sid 리스트 획득
-#rooms =  defaultdict(dict) # rooms[roomnname][sid] = name    candidate , ice  
-sid_2_rooms=  {}  # sid 를 통한 방 추적  sid_2_rooms[sid] = roomname
-sid_2_tutor = []
-             
-file_lock = threading.Lock()
+#매니저 가져오기
+manager = sio.manager
+
+# 방, 참여자, 참여자 정보를 2중 딕셔너리로 저장
+rooms : defaultdict[str, dict[str, str]] = defaultdict(dict[str, str]) 
+# rooms[roomnname][sid] = offer
+# roomnname : 방이름 (사용자지정 문자열)
+# sid : 소켓 id (해쉬값 문자열)
+# offer : 소켓 정보?
+
+# sid값으로 해당 소켓이 참여중인 방의 이름을 바로 알 수 있게 저장
+sid_2_rooms : dict[str, str] = {}  
+# 강의자는 별도로 저장해두기 
+tutors : list[str] = []
+
+# 공유자원 처리하려고 만든거라는데 필요 없다나? 신경 ㄴㄴ
+file_lock : threading.Lock = threading.Lock()
 
 #==============================get============================================
-def get_room_list():  # 저장 된 방 리스트 반환
+def get_room_list():  # X > 리스트(모든 방의 이름)
     return list(rooms.keys())
  
-def get_roommember_list(roomname): #저장된 방에 있는  멤버 sid 리스트 반환
-    if roomname in get_room_list():
-        return list(rooms[roomname].keys())
-    else:
-        getrooms=rooms[roomname] = []
-        return getrooms
+def get_roommember_list(roomname): # 방 이름 > 리스트(그 방의 모든 참여자 ID)
+    if not roomname in get_room_list():
+        rooms[roomname] = []
+    return list(rooms[roomname].keys())
 
+#공사중
 def get_room_sid_ice(roomname, sid): #저장된 방에 있는 멤버 sid 에 있는 정보 반환
     if sid in get_roommember_list(roomname):
         return rooms[roomname][sid]
     else:
         return None
 
-def get_room_sid(sid):   # sid를통한 방 추출 
-    if sid in sid_2_rooms.keys():
-        return sid_2_rooms[sid]
-    else:
-        return None
+def get_roomname_by_sid(sid):   # 참여자 ID > 참여중인 방 이름 
+    return sid_2_rooms[sid]
 
 #==============================set============================================
-def save_rooms_info(roomname , sid , file):
-    rooms[roomname][sid] = file
-
-def save_roomuser_sid(roomname , sid): # sid 에 따른 방이름 저장
-    sid_2_rooms[sid] = roomname
- 
+def save_offer_in_room(sid , offer): # 참여자 아이디에 해당하는 참여자 정보 저장
+    rooms[sid_2_rooms[sid]][sid] = offer
 
 #==============================control=============================================
 
 #delete method
-def remove_user_from_room( roomname, sid):
-    room_data = rooms.get(roomname)
-    if room_data and sid in room_data:
-        del room_data[sid]
+def remove_user_from_room(roomname, sid): # 방이름으로 방 안의 참여자 제거.
+    room = rooms.get(roomname)
+    if room is None: # roomname을 가진 room이 존재 and 해당 room에 sid이 존재.
+        return False
+    if not sid in room :
+        return False
+    
+    del room[sid]
+    return True
  
 
 #==============================socketio property===============================
 @app.get('/')
 async def index():
-    
     return FileResponse('fiddle.html')
 
 @sio.on('connect')
-async def coonnected(sid,*args, **kwargs):     
+async def connected(sid,*args, **kwargs):     
     await sio.emit("fuckshit", list(rooms),to=sid) # 접속 시 모든 방에 대한 리스트 줌 방 보기  
     
     
 @sio.on('join_room')
-def joinroom(sid,*args, **kwargs): #1 인자 : 방이름 , #2인자 자료 
-    if args[0] not in get_room_list(): # 없는 방이라면 생성되니 add 이벤트 
-        sio.emit('roomadd',args[0]) 
-        sid_2_tutor.append(sid)
-    if args[0] in get_room_list(): # 있는 방이라면 방유저들에게 연결 이벤트 처리 
-        sio.emit('user-connect', sid, room=args[0]) # 기존 유저들이 sid 를 추가 하기 위한  자들어올때 방이름 받고   방에 없으면 안감
-    sio.emit('connected',get_roommember_list(args[0]), to= sid) # 해당 방안에 있는 리스트 모든 클라이언트 리스트  # 함수내부 있으면 방 리스트 리턴 없으면 생성 후 공백배열 리턴 
-    rooms[args[0]][sid] = None # 초대장?  추가 (rooms[방이름][sid번호] =  클라이언트 정보 )
-    sid_2_rooms[sid] = args[0] # sid  to  room 추가 (sid 를 통한 방이름 추출 ) 
-    sio.enter_room(sid=sid ,  room= args[0]) # 방안에 넣기  맨 나중에 한 이유는 리스트를 줄때 본인을 제외하고 주기 위함 
-     
+def joinroom(sid,*args, **kwargs): #1 인자 : 방이름 
+    # 방이름 받아옴
+    roomName : str = args[0]
+
+    # 방 있는지 없는지 없으면 생성
+    if roomName not in get_room_list(): # 없는 방이라면 생성되니 add 이벤트 
+        sio.emit('roomadd',roomName) 
+        tutors.append(sid)
+    else : # 있는 방이라면 방유저들에게 연결 이벤트 처리 
+        sio.emit('user-connect', sid, room=roomName) # 기존 유저들이 sid 를 추가 하기 위한  자들어올때 방이름 받고   방에 없으면 안감
+    
+    # [후처리]
+    # 해당 방안에 있는 리스트 모든 클라이언트 리스트 
+    # 함수내부 있으면 방 리스트 리턴 없으면 생성 후 공백배열 리턴 
+    sio.emit('connected', get_roommember_list(roomName), to = sid) 
+    # 초대장?  추가 (rooms[방이름][sid번호] =  클라이언트 정보 )
+    rooms[roomName][sid] = None 
+    # sid  to  room 추가 (sid 를 통한 방이름 추출 ) 
+    sid_2_rooms[sid] = roomName 
+    # 방안에 넣기  맨 나중에 한 이유는 리스트를 줄때 본인을 제외하고 주기 위함 
+    sio.enter_room(sid=sid ,  room= roomName) 
      
     
 @sio.on('disconnect')
-def disconnected(sid,*args, **kwargs):
-    if sid_2_rooms.get(sid):
-        sio.emit('user-disconnect',sid,to=sid_2_rooms[sid]) #나간사람 통지 방에 있는 사람들에게
+def disconnected(sid,*args, **kwargs) :
+    # 방안에 있는지 확인해본다.
+    isInRoom : bool = sid_2_rooms.get(sid) is not None
     roomname = sid_2_rooms.get(sid)
-    if roomname: 
-        sio.leave_room(sid=sid,room=roomname)
+
+    # 방안에 있었다면
+    if isInRoom: 
+        #방에 남은 사람들에게 해당 유저의 퇴장을 통지
+        sio.emit('user-disconnect', sid, to=roomname) 
+        
+        #sid를 roomname에서 내보냄
+        sio.leave_room(sid, roomname)
         remove_user_from_room(roomname=roomname, sid= sid)
         sid_2_rooms.pop(sid,None)
+
         if len(rooms[roomname].keys())== 0:
             rooms.pop(roomname,None)
             manager.close_room(room=roomname)
             sio.emit('roomremove',roomname)
+
+    # roomname이 없어도 해준데요~
     rooms[roomname].pop(sid,None)
     sid_2_rooms.pop(sid,None)
     #sid_2_tutor.pop(sid,None)
@@ -148,7 +166,7 @@ def offer(sid,*args, **kwargs):
     roomname   = args[1]  # 방이름  
     data  = args[2]
     rooms[roomname][sid]
-    if sid in sid_2_tutor: # 튜터인 경우  
+    if sid in tutors: # 튜터인 경우  
         with file_lock:
             with open(f'{sid}.wav', 'ab') as f: #서버에서 저장할 폴더에 이제 sid 이름으로 이게 첫 유저면 저장 
                 f.write(data)
@@ -177,7 +195,6 @@ def answer(sid,*args, **kwargs):
     roomname = args[1]
     sio.emit('answer',answer,room= roomname)
 
-
 sio.on('ice')
 def ice(sid, *args, **kwargs):
     ice  = args[0]
@@ -187,10 +204,7 @@ def ice(sid, *args, **kwargs):
 @sio.on('roomchange') # 필요하다면 구현 
 async def roomchanged(sid,*args, **kwargs):
     return "D"
-    
-     
 
-    
+
 if __name__ == '__main__':
-
     uvicorn.run(combined_asgi_app, host='127.0.0.1', port=5000)
---
+++ Python-Javascript-Websocket-Video-Streaming--main/static/fiddle.js
@@ -0,0 +1,25 @@
+'use strict';
+
+(function() {
+
+  const socket = io();
+
+  socket.on('connect', (data)=> {
+    console.log(`connect ${socket.id}`);
+    console.log(data);
+  });
+
+  socket.on('disconnect',  () => {
+    console.log(`disconnect ${socket.id}`);
+   
+  });
+
+  socket.on('hello', (a, b, c) => {
+    console.log(a, b, c);
+  });
+  socket.on('fuckshit', (a) => {
+    console.log(a);
+  });
+
+
+})();
